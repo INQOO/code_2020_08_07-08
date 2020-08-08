d465e3f6699018266ed61931093216db52e0137a
Olaf Goj <olaf@goj.email>
2020-08-08 12:33:06 +0200

    additional instructions

--- src/streams/assignments/Assignment1.java
+++ src/streams/assignments/Assignment1.java
@@ -10,6 +10,7 @@ import static java.util.Collections.emptyList;
 public class Assignment1 {
     public static void main(String[] args) {
         String[] words = new String[]{"yinqoo", "in", "yy", "y", "yellow", "cloudy", "yjavay"};
+        // znajduje slowa ktore zaczynaja sie i koncza litera "y"
         // ponizsze powinno wypisac: [yy, y, yjavay]
         System.out.println(findY(words));
     }
